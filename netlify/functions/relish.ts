42217fdba63e854763854a80639cd4864aecf757
wirthschwind <139864045+wirthschwind@users.noreply.github.com>
2024-01-12 17:09:36 +0100

    Update relish.ts

--- netlify/functions/relish.ts
+++ netlify/functions/relish.ts
@@ -2,7 +2,7 @@ import type { Context } from "@netlify/functions"
 
 export default async (req: Request, context: Context) => {
   let response;
-  const body = await req.json();
+  const body = await req.text();
   const header = await req.headers;
   switch(req?.method){
     case "POST":
