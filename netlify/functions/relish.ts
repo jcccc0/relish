094b85427d64e44acfa0c2b88562edf0e62297a7
wirthschwind <139864045+wirthschwind@users.noreply.github.com>
2024-01-12 16:23:50 +0100

    Update relish.ts

--- netlify/functions/relish.ts
+++ netlify/functions/relish.ts
@@ -3,5 +3,5 @@ import type { Context } from "@netlify/functions"
 export default async (req: Request, context: Context) => {
   const body = req?.body;
   const header = req?.headers;
-  return new Response("Welcome relish!", body, header)
+  return new Response(JSON.stringify({body, header}))
 }
